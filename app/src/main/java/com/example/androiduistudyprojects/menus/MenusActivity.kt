f257c5ef633ff204e4196ddef2c2f366521c1d89
Youhei Oonuma <oonuma617@gmail.com>
2020-04-22 20:57:44 +0900

    add popup menu

--- app/src/main/java/com/example/androiduistudyprojects/menus/MenusActivity.kt
+++ app/src/main/java/com/example/androiduistudyprojects/menus/MenusActivity.kt
@@ -3,6 +3,7 @@ package com.example.androiduistudyprojects.menus
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androiduistudyprojects.R
 import com.google.android.material.snackbar.Snackbar
@@ -14,6 +15,27 @@ class MenusActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menus)
+
+        button_1.setOnClickListener {
+            PopupMenu(this, it).apply {
+                menuInflater.inflate(R.menu.custom_menu, menu)
+                setOnMenuItemClickListener { item ->
+                    when (item.itemId) {
+                        R.id.search -> {
+                            Snackbar.make(it, "Click search", Snackbar.LENGTH_SHORT).show()
+                        }
+                        R.id.help -> {
+                            Snackbar.make(it, "Click help", Snackbar.LENGTH_SHORT).show()
+                        }
+                        else -> {
+                            Snackbar.make(it, "non reach", Snackbar.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    return@setOnMenuItemClickListener true
+                }
+            }.show()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
